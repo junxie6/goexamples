cbce784ac901b96314f9e91ce53fac5201bcef6c
Jun Hsieh <jun.github@ijun.org>
2016-04-30 22:18:50 -0700

    up

--- util/util.go
+++ util/util.go
@@ -28,9 +28,7 @@ func ReadWebContent(url string) ([]byte, error) {
 		return nil, err
 	}
 
-	// We must close resp.Body on all execution paths.
 	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
 		return nil, fmt.Errorf("StatusCode: %s", resp.Status)
 	}
 
