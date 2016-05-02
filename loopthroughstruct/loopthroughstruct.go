12ca5ecb1010bde7e3752063aa940f0fd19e50b8
Jun Hsieh <jun.github@ijun.org>
2016-05-02 16:24:37 -0700

    up

--- loopthroughstruct/loopthroughstruct.go
+++ loopthroughstruct/loopthroughstruct.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
-type person struct {
+type Employee struct {
 	Status    bool
 	Firstname string
-	Lastname  string
+	LastName  string
 	Age       int
 }
 
 func main() {
-	typ := reflect.TypeOf(person{})
+	// this example shows how to loop through the field names of the struct and set field value by field name.
+	emp := Employee{}
 
-	fmt.Printf("%v\n", typ)
+	empFields := reflect.TypeOf(Employee{})
 
-	for i := 0; i < typ.NumField(); i++ {
-		fmt.Printf("%v %v\n", typ.Field(i).Name, typ.Field(i).Type)
+	for i := 0; i < empFields.NumField(); i++ {
+		fieldName := empFields.Field(i).Name
+		fieldType := empFields.Field(i).Type
+
+		fmt.Printf("%s\t%s\n", fieldName, fieldType)
+
+		if fieldName == "Status" || fieldName == "Age" {
+			continue
+		}
+
+		fieldVal := "Test " + strconv.Itoa(i)
+
+		reflect.ValueOf(&emp).Elem().FieldByName(fieldName).SetString(fieldVal)
+	}
+
+	empJSON, err := json.MarshalIndent(emp, "", " ")
+
+	if err == nil {
+		fmt.Printf("%s\n", empJSON)
 	}
 }
