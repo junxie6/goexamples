71a364764f9cbd938268c9bf12bf9a63804feebd
Jun Hsieh <jun.github@ijun.org>
2016-05-01 21:31:03 -0700

    up

--- util/util.go
+++ util/util.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -50,4 +51,21 @@ func DaysAgo(t time.Time) int {
 func ConvStrToTime(str string) (time.Time, error) {
 	layout := "2006-01-02" // Mon Jan 2 15:04:05 -0700 MST 2006
 	return time.Parse(layout, str)
+}
+
+// WaitForServer attempts to contact the server of a URL.
+// It tries for one minute using exponential backoff.
+// It reports an error if all attempts fail.
+func WaitForServer(url string) error {
+	const timeout = 1 * time.Minute
+	deadline := time.Now().Add(timeout)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Head(url)
+		if err == nil {
+			return nil // success
+		}
+		log.Printf("server not responding (%s); retrying...", err)
+		time.Sleep(time.Second << uint(tries)) // exponential backoff
+	}
+	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
 }
