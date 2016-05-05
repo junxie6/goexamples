b9db383a3751c7c29d1c9a804027e6d0847b9d06
Jun Hsieh <jun.github@ijun.org>
2016-05-05 10:35:47 -0700

    up

--- convstructjson/convstructjson.go
+++ convstructjson/convstructjson.go
@@ -103,6 +103,7 @@ func getMyOrder() MyOrder {
 
 func exampleConvStructToJSONOutputBrowser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	//w.WriteHeader(http.StatusOK)
 
 	order1 := getMyOrder()
 	encoder := json.NewEncoder(w)
