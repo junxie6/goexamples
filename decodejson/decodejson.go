4baf5a12a3ca58ccb1d6c6a0a96f9484fb4f1ee1
Jun Hsieh <jun.github@ijun.org>
2016-05-14 10:50:45 -0700

    up

--- decodejson/decodejson.go
+++ decodejson/decodejson.go
@@ -5,26 +5,43 @@ import (
 	"fmt"
 )
 
+// convJSONToMap shows how to decode the nested JSON string into a map by doing type assertion.
+// Without type assertion, you will get invalid operation: (type interface {} does not support indexing)
 func convJSONToMap() {
 	strJSON := `{"100":{"DealerName":"Company A","DealerCountry":"USA","ItemArr":[{"LineNum":0,"IdItem":10,"ItemNum":"A0"},{"LineNum":1,"IdItem":11,"ItemNum":"A1"}]},"101":{"DealerName":"Company B","DealerCountry":"Canada","ItemArr":[{"LineNum":0,"IdItem":10,"ItemNum":"A0"},{"LineNum":1,"IdItem":11,"ItemNum":"A1"}]}}`
 
 	var data map[string]interface{}
 
-	_ = json.Unmarshal([]byte(strJSON), &data)
+	if err := json.Unmarshal([]byte(strJSON), &data); err != nil {
+	} else {
+		for IdDealer, v0 := range data {
+			DealerName := v0.(map[string]interface{})["DealerName"]
+			DealerCountry := v0.(map[string]interface{})["DealerCountry"]
 
-	for IdDealer, v0 := range data {
-		DealerName := v0.(map[string]interface{})["DealerName"]
-		DealerCountry := v0.(map[string]interface{})["DealerCountry"]
+			fmt.Printf("%v\t%v\t%v\n", IdDealer, DealerName, DealerCountry)
 
-		fmt.Printf("%v\t%v\t%v\n", IdDealer, DealerName, DealerCountry)
+			ItemArr := v0.(map[string]interface{})["ItemArr"]
 
-		ItemArr := v0.(map[string]interface{})["ItemArr"]
+			for k1, v1 := range ItemArr.([]interface{}) {
+				LineNum := v1.(map[string]interface{})["LineNum"]
+				IdItem := v1.(map[string]interface{})["IdItem"]
+				ItemNum := v1.(map[string]interface{})["ItemNum"]
+				fmt.Printf("\tLine[%v]:\t%v\t%v\t%v\n", k1, LineNum, IdItem, ItemNum)
+			}
 
-		for k1, v1 := range ItemArr.([]interface{}) {
-			LineNum := v1.(map[string]interface{})["LineNum"]
-			IdItem := v1.(map[string]interface{})["IdItem"]
-			ItemNum := v1.(map[string]interface{})["ItemNum"]
-			fmt.Printf("\tLine[%v]:\t%v\t%v\t%v\n", k1, LineNum, IdItem, ItemNum)
+			// If the underlying type is unknown, a type switch determines the type:
+			switch v := ItemArr.(type) {
+			case int:
+				fmt.Printf("It's int: %v\n", v)
+			case float64:
+				fmt.Printf("It's float64: %v\n", v)
+			case string:
+				fmt.Printf("It's string: %v\n", v)
+			case []interface{}:
+				fmt.Printf("It's []interface{}: %v\n", v)
+			default:
+				fmt.Printf("It is not one of the types above: %v\n", v)
+			}
 		}
 	}
 }
