5c13f3b7e7ca008c8b9af1f7f0a230eafe73b215
Jun Hsieh <jun.github@ijun.org>
2016-05-23 08:33:39 -0700

    up

--- test2.go
+++ test2.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
-	// 我應該看到這個訊息
+import (
+	"fmt"
+)
+
+func test() string {
+	return "hello world"
 }
+
+func InArrayInt(v int, vArr []int) bool {
+	for _, vv := range vArr {
+		if v == vv {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Printf(test())
+}
+
+// 任何 language，都逃不出以下幾個東西：
+// 1: loop (迴圈)
+// 2. if else (判斷)
+// 3. variable (變數)
+// 4. constant
+// 5. function
