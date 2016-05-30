386ba35f294f7164614f6532eb20fbb782ce0439
Jun Hsieh <jun.github@ijun.org>
2016-05-30 14:26:43 -0700

    up

--- connectmysqlmodel/model/model.go
+++ connectmysqlmodel/model/model.go
@@ -40,3 +40,62 @@ func txEnd(tx *sql.Tx, errArrPtr *[]error) {
 		*errArrPtr = append(*errArrPtr, err)
 	}
 }
+
+// txWrapper usage example:
+//so := SO{}
+//
+//ifArr := []interface{}{
+//	&so,
+//}
+//
+//err := txWrapper(func(tx *sql.Tx, ifArr []interface{}) error {
+//	if err := ifArr[0].(*SO).lockSO(tx); err != nil {
+//		return err
+//	}
+//
+//	return nil
+//}, ifArr)
+
+func txWrapper(txFunc func(*sql.Tx, []interface{}) error, ifArr []interface{}) (retErr error) {
+	tx, retErr := db.Begin()
+
+	if retErr != nil {
+		return
+	}
+
+	// Although tx could be accessed inside the defer function
+	// it is good practice to pass it through argument. Because:
+	// "deferred function's arguments are evaluated when the defer statement is evaluated."
+	// "…they may refer to variables defined in a surrounding function.
+	// Those variables are then shared between the surrounding function and the function literal, and they survive as long as they are accessible."
+	// http://www.blevesearch.com/news/Deferred-Cleanup,-Checking-Errors,-and-Potential-Problems/
+	// https://blog.golang.org/defer-panic-and-recover
+	defer func(tx *sql.Tx) {
+		// If the current goroutine is panicking, a call to recover will capture the value given to panic and resume normal execution.
+		// We capcture the error before making the final decision (rollback/commit).
+		if err := recover(); err != nil {
+			switch v := err.(type) {
+			case error:
+				retErr = v
+			case string:
+				retErr = errors.New(v)
+			default:
+				retErr = fmt.Errorf("%v", v)
+			}
+		}
+
+		if retErr != nil {
+			// Do not try to assign the return value of tx.Rollback to err directily.
+			// If you do, you would invert the meaning of the final return value.
+			// Because if Rollback succeeds, it would return nil.
+			if err := tx.Rollback(); err != nil {
+				retErr = err
+			}
+			return
+		}
+
+		retErr = tx.Commit()
+	}(tx)
+
+	return txFunc(tx, ifArr) // the return value of txFunc will be set to retErr.
+}
