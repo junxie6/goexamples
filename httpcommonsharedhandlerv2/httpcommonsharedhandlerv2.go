c6d69b03fc5363aba66cd0c74223fbd554d5fcce
Jun Hsieh <jun.github@ijun.org>
2016-06-01 14:17:10 -0700

    up

--- httpcommonsharedhandlerv2/httpcommonsharedhandlerv2.go
+++ httpcommonsharedhandlerv2/httpcommonsharedhandlerv2.go
@@ -8,37 +8,6 @@ import (
 	"strings"
 )
 
-func isAllowedHost(proxyIPPort string, userIP string) bool {
-	// In case of IPv4 most client addresses are masked behind NAT, on your server side you ONLY see the globally routable address
-	// which is the router's own global address.
-	// Be caution against using the X-Forwarded-For header for ANYTHING unless it comes from a trusted source (e.g. your own reverse proxy).
-	// The client can set this header to an arbitrary value and can cause some funny or even dangerous bugs to be triggered.
-	allowProxyIP := "192.168.0.1"
-
-	//test := proxy_ip[0:strings.Index(proxy_ip, ":")]
-	proxyIPPortArr := strings.Split(proxyIPPort, ":")
-
-	if proxyIPPortArr[0] != allowProxyIP {
-		log.Printf("Disallow Proxy IP: %s", proxyIPPortArr[0])
-		return false
-	}
-
-	//return true // remove the user's IP restriction
-
-	allowHostArr := []string{
-		"192.168.0.2",
-	}
-
-	for _, v := range allowHostArr {
-		if userIP == v {
-			return true
-		}
-	}
-
-	log.Printf("Disallow user IP: %s", userIP)
-	return false
-}
-
 // Reference:
 // http://stackoverflow.com/questions/26204485/gorilla-mux-custom-middleware
 // http://laicos.com/writing-handsome-golang-middleware/
@@ -88,6 +57,37 @@ func serveTest2(w http.ResponseWriter, r *http.Request) error {
 func serveTest3(w http.ResponseWriter, r *http.Request) error {
 	w.Write([]byte("Test3"))
 	return nil
+}
+
+func isAllowedHost(proxyIPPort string, userIP string) bool {
+	// In case of IPv4 most client addresses are masked behind NAT, on your server side you ONLY see the globally routable address
+	// which is the router's own global address.
+	// Be caution against using the X-Forwarded-For header for ANYTHING unless it comes from a trusted source (e.g. your own reverse proxy).
+	// The client can set this header to an arbitrary value and can cause some funny or even dangerous bugs to be triggered.
+	allowProxyIP := "192.168.0.1"
+
+	//test := proxy_ip[0:strings.Index(proxy_ip, ":")]
+	proxyIPPortArr := strings.Split(proxyIPPort, ":")
+
+	if proxyIPPortArr[0] != allowProxyIP {
+		log.Printf("Disallow Proxy IP: %s", proxyIPPortArr[0])
+		return false
+	}
+
+	//return true // remove the user's IP restriction
+
+	allowHostArr := []string{
+		"192.168.0.2",
+	}
+
+	for _, v := range allowHostArr {
+		if userIP == v {
+			return true
+		}
+	}
+
+	log.Printf("Disallow user IP: %s", userIP)
+	return false
 }
 
 func main() {
