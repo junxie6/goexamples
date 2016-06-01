05e1bbc2264daedeeebf76c3e84bec940ef81347
Jun Hsieh <jun.github@ijun.org>
2016-06-01 11:08:12 -0700

    up

---
+++ httpcommonsharedhandler/httpcommonsharedhandler.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/NYTimes/gziphandler"
+	"log"
+	"net/http"
+)
+
+func serveTest1(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Test1"))
+}
+
+func serveTest2(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Test2"))
+}
+
+func serveCommon(h http.Handler) http.Handler {
+	return gziphandler.GzipHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("middleware: ", r.URL)
+
+		h.ServeHTTP(w, r)
+	}))
+}
+
+func main() {
+	http.Handle("/test1", serveCommon(http.HandlerFunc(serveTest1)))
+	http.Handle("/test2", serveCommon(http.HandlerFunc(serveTest2)))
+
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
