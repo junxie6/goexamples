8d67a2868b347f912b2c5118811d3bf231c3b771
Jun Hsieh <jun.github@ijun.org>
2016-08-25 10:59:08 -0700

    Update

--- httpmuxmiddleware/exp.go
+++ httpmuxmiddleware/exp.go
@@ -16,36 +16,55 @@ func srvHome(w http.ResponseWriter, r *http.Request) {
 func something() {
 }
 
+//
 func HelloHandler(h1 http.Handler) http.Handler {
+	log.Printf("Inside Hello")
 	return func(h2 http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello Before\n"))
+			log.Printf("Hello Before")
+			w.Write([]byte("Hello Before " + r.FormValue("act") + "\n"))
+
 			h2.ServeHTTP(w, r)
-			w.Write([]byte("Hello After\n"))
+
+			log.Printf("Hello After")
+			w.Write([]byte("Hello After " + r.FormValue("act") + "\n"))
 		})
 	}(h1)
 }
 
 func WorldHandler(h1 http.Handler) http.Handler {
+	log.Printf("Inside World")
 	return func(h2 http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("World Before\n"))
+			log.Printf("World Before")
+			w.Write([]byte("World Before " + r.FormValue("act") + "\n"))
+
 			h2.ServeHTTP(w, r)
-			w.Write([]byte("World After\n"))
+
+			w.Write([]byte("World After " + r.FormValue("act") + "\n"))
+			log.Printf("World After")
 		})
 	}(h1)
 }
 
 func ThereHandler(h1 http.Handler) http.Handler {
+	log.Printf("Inside There")
 	return func(h2 http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("There Before\n"))
+			log.Printf("There Before")
+			w.Write([]byte("There Before " + r.FormValue("act") + "\n"))
+
 			h2.ServeHTTP(w, r)
-			w.Write([]byte("There After\n"))
+
+			log.Printf("There After")
+			w.Write([]byte("There After " + r.FormValue("act") + "\n"))
 		})
 	}(h1)
 }
 
+//
+
+//
 func s1(s string) string {
 	log.Printf("s1")
 	return "s1"
@@ -60,7 +79,9 @@ func s3(s string) string {
 }
 
 func main() {
-	http.Handle("/", HelloHandler(WorldHandler(ThereHandler(http.HandlerFunc(srvHome)))))
+	http.Handle("/", http.HandlerFunc(srvHome))
+	http.Handle("/Hello", HelloHandler(WorldHandler(ThereHandler(http.HandlerFunc(srvHome)))))
+	http.Handle("/How", http.HandlerFunc(srvHome))
 	http.ListenAndServe(":8080", nil)
 
 	//log.Printf("Here: %v", s1(s2(s3("asdf"))))
