b0b0bedeae1ef1021d70253619572fc7111dece7
Jun Hsieh <jun.github@ijun.org>
2016-12-30 20:02:05 -0800

    Update

---
+++ readgzippedresponse/readgzippedresponse.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// Reference:
+// https://www.socketloop.com/tutorials/golang-read-gzipped-http-response
+func main() {
+	client := new(http.Client)
+
+	request, err := http.NewRequest("Get", "http://blog.ijun.org/", nil)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	request.Header.Add("Accept-Encoding", "gzip")
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	// Check that the server actual sent compressed data
+	var reader io.ReadCloser
+
+	fmt.Printf("Content-Encoding: %v\n", response.Header.Get("Content-Encoding"))
+
+	switch response.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, err = gzip.NewReader(response.Body)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer reader.Close()
+	default:
+		reader = response.Body
+	}
+
+	// to standard output
+	_, err = io.Copy(os.Stdout, reader)
+
+	// see https://www.socketloop.com/tutorials/golang-saving-and-reading-file-with-gob
+	// on how to save to file
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
