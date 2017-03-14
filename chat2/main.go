9b05c45fa6f403efca47340145b5b5095ec58377
Jun Hsieh <jun.github@ijun.org>
2017-03-13 20:52:38 -0700

    Add client, room

--- chat2/main.go
+++ chat2/main.go
@@ -3,21 +3,38 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-			<head>
-			</head>
-			<body>
-			hi
-			</body>
-		</html>
-		`))
-	})
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte(`
+	//	`))
+	//})
+
+	room := newRoom()
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", room)
+
+	go room.run()
+
+	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
