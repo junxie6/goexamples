6fda49137dde80bdafd0bbce8066e285a6c4c869
Jun Hsieh <jun.github@ijun.org>
2017-03-16 05:07:39 -0700

    Add timeout

--- rpc/cli.go
+++ rpc/cli.go
@@ -4,17 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"net/rpc"
 	"os"
+	"time"
 )
 
 func main() {
+	// Connection without setting a timeout:
 	conn, err := rpc.Dial("tcp", "localhost:8080")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// If you want to add timeout:
+	//conn0, err := net.DialTimeout("tcp", "localhost:8080", 5*time.Second)
+
+	//if err != nil {
+	//	log.Fatal("dialing:", err)
+	//}
+
+	//conn := rpc.NewClient(conn0)
+
+	//
 	in := bufio.NewReader(os.Stdin)
 
 	for {
