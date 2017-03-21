c8f1b4745f77e240ef580690165474f93c840874
Jun Hsieh <jun.github@ijun.org>
2017-03-21 06:02:51 -0700

    Refactor the code

--- goredis/goredis.go
+++ goredis/goredis.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"github.com/go-redis/redis"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+var (
+	client *redis.ClusterClient
 )
 
 func main() {
@@ -14,28 +25,41 @@ func main() {
 	//	DB:       0,                 // use default DB
 	//})
 
-	client := redis.NewClusterClient(&redis.ClusterOptions{
+	client = redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs: []string{":30001", ":30002", ":30003", ":30004", ":30005", ":30006"},
 	})
 
 	pong, err := client.Ping().Result()
 	fmt.Printf("Pong: %#v and %#v\n", pong, err)
 
-	var str string
+	DemoString()
+	DemoStruct()
+}
 
-	str, err = client.Get("hello").Result()
-
-	if err != nil {
+func DemoString() {
+	if str, err := client.Get("hello").Result(); err != nil {
 		fmt.Printf("err: %s\n", err)
+	} else {
+		fmt.Printf("hello: %s\n", str)
+	}
+}
+
+func DemoStruct() {
+	p1 := Person{Name: "Jun", Age: 19}
+	buf := new(bytes.Buffer)
+	gob.NewEncoder(buf).Encode(&p1)
+
+	if err := client.HSet("test1", "person", buf.Bytes()).Err(); err != nil {
+		fmt.Printf("HSet err: %s\n", err)
+		return
 	}
 
-	fmt.Printf("hello: %s\n", str)
-
-	str, err = client.Get("foo").Result()
-
-	if err != nil {
-		fmt.Printf("err: %s\n", err)
+	if b, err := client.HGet("test1", "person").Bytes(); err != nil {
+		fmt.Printf("HGet err: %s\n", err)
+		return
+	} else {
+		p2 := Person{}
+		gob.NewDecoder(bytes.NewReader(b)).Decode(&p2)
+		fmt.Printf("Person: %#v\n", p2)
 	}
-
-	fmt.Printf("foo: %s\n", str)
 }
