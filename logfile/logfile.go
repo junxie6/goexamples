156c90d44bae498204de9cdb129047032c7061bb
Jun Hsieh <jun.github@ijun.org>
2017-05-12 21:28:55 -0700

    Save the log output to a file globally

---
+++ logfile/logfile.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+)
+
+func logfile_example1(fileName string) (*os.File, error) {
+	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Set output to file.
+	log.SetOutput(f)
+
+	//log.SetOutput(os.Stderr) // to standard error (default).
+	//log.SetOutput(os.Stdout) // to standard output.
+
+	// Optional: include line number where the log is called.
+	log.SetFlags(log.LstdFlags | log.Llongfile) // or log.Lshortfile.
+
+	return f, nil
+}
+
+func logfile_example2(fileName string) (*os.File, error) {
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+
+	if err != nil {
+		return nil, err
+	}
+
+	syscall.Dup2(int(f.Fd()), 1) /* -- stdout */
+	syscall.Dup2(int(f.Fd()), 2) /* -- stderr */
+
+	// Optional: include line number where the log is called.
+	log.SetFlags(log.LstdFlags | log.Llongfile) // or log.Lshortfile.
+
+	return f, nil
+}
+
+func main() {
+	fileName := "test.log"
+
+	// example 1
+	//logFilePtr, err := logfile_example1(fileName)
+
+	// example 2
+	logFilePtr, err := logfile_example2(fileName)
+
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		return
+	}
+
+	defer logFilePtr.Close()
+
+	// log
+	log.Println("This is a test log entry")
+}
