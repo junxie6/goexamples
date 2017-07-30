303d1503a38a4e4839908e44c94757d6ce64cfee
Jun Hsieh <jun.github@ijun.org>
2017-07-29 22:55:49 -0700

    update

--- print_byte_read/print_byte_read.go
+++ print_byte_read/print_byte_read.go
@@ -1,4 +1,6 @@
 // Display each byte read while reading the data
+// Reference:
+// https://stackoverflow.com/questions/22421375/how-to-print-the-bytes-while-the-file-is-being-downloaded-golang
 package main
 
 import (
@@ -23,9 +25,9 @@ func UseStdLibExample() {
 	src = bytes.NewBufferString("Some random input data")
 
 	//
-	r := io.TeeReader(src, &debugBuf)
+	src = io.TeeReader(src, &debugBuf)
 
-	dst, err := ioutil.ReadAll(r)
+	dst, err := ioutil.ReadAll(src)
 
 	if err != nil {
 		log.Fatal(err)
@@ -36,8 +38,59 @@ func UseStdLibExample() {
 	spew.Dump(debugBuf.Bytes())
 }
 
-func main() {
+// PrintRead wraps an existing io.Reader.
+//
+// It simply forwards the Read() call, while displaying
+// the results from individual calls to it.
+type PrintRead struct {
+	reader   io.Reader
+	debugBuf bytes.Buffer
+	total    int64 // Total # of bytes transferred
+}
 
+// Read 'overrides' the underlying io.Reader's Read method.
+// This is the one that will be called by io.Copy(). We simply
+// use it to keep track of byte counts and then forward the call.
+func (pr *PrintRead) Read(p []byte) (int, error) {
+	n, err := pr.reader.Read(p)
+
+	if n > 0 {
+		pr.total += int64(n)
+
+		if n, err := pr.debugBuf.Write(p[:n]); err != nil {
+			return n, err
+		}
+	}
+
+	return n, err
+}
+
+func UseCustomStruct() {
+	var src io.Reader // Source file/url/etc
+
+	// Create some random input data.
+	src = bytes.NewBufferString("Some random input data")
+
+	pr := &PrintRead{
+		reader: src,
+	}
+
+	dst, err := ioutil.ReadAll(pr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n\n", dst)
+
+	spew.Dump((*pr).debugBuf.Bytes())
+	fmt.Printf("Total bytes: %d\n", (*pr).total)
+}
+
+func main() {
 	// Use Go's stdlib io.TeeReader
 	UseStdLibExample()
+
+	// Use a custom struct
+	UseCustomStruct()
 }
