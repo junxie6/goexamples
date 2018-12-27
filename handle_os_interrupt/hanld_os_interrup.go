ec88be9dbef458f5402e09c06cfa87dfd7bb8912
Jun Hsieh <jun.github@ijun.org>
2018-12-26 16:27:54 -0800

    improve handling OS signals

--- handle_os_interrupt/hanld_os_interrup.go
+++ handle_os_interrupt/hanld_os_interrup.go
@@ -1,7 +1,9 @@
 package main
 
 // Reference:
+// https://gobyexample.com/signals
 // https://stackoverflow.com/questions/11268943/is-it-possible-to-capture-a-ctrlc-signal-and-run-a-cleanup-function-in-a-defe?rq=1
+// https://stackoverflow.com/questions/13107958/what-exactly-does-runtime-gosched-do
 
 import (
 	"fmt"
@@ -11,17 +13,42 @@ import (
 	"time" // or "runtime"
 )
 
-func cleanup() {
-	fmt.Println("doing cleanup")
+func cleanup(exitStatus chan int) {
+	fmt.Println("Please wait. Doing some cleanup tasks 1/3")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Please wait. Doing some cleanup tasks 2/3")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Please wait. Doing some cleanup tasks 3/3")
+	time.Sleep(1 * time.Second)
+
+	// TODO: implement the logic to determine the exit status
+	exitStatus <- 0
 }
 
 func main() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		<-c
-		cleanup()
-		os.Exit(1)
+		// Make a buffered channel of size one, so the notifier will not be blocked
+		sigs := make(chan os.Signal, 1)
+		exitStatus := make(chan int, 1)
+
+		// Register the given channel to receive notifications of the specified signals.
+		signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+
+		// awaiting the signal
+		<-sigs
+
+		// Catch and handle Unix signals
+		go cleanup(exitStatus)
+
+		// Give the process a chance to cleanup itself.
+		select {
+		case <-time.After(10 * time.Second):
+			fmt.Printf("Time is up. Exitting now.\n")
+			os.Exit(1)
+		case code := <-exitStatus:
+			fmt.Printf("Exit status: %v\n", code)
+			os.Exit(code)
+		}
 	}()
 
 	for {
